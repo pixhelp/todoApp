d30d51c6f83232c0be79c66b33bfcc2b6484a94c
pixhelp <openofmind@gmail.com>
2025-05-28 00:21:41 +0200

    add title in object todo

--- src/components/CardTodo.tsx
+++ src/components/CardTodo.tsx
@@ -6,7 +6,7 @@ type CardTodoProps = {
 
 const CardTodo = ({children}: CardTodoProps) => {
     return (
-         <div className="bg-white text-gray-800 rounded shadow-md w-full max-w-md">
+         <div className="bg-[var(--color-bg-light)] text-gray-800 rounded shadow-md w-full max-w-md">
             {children}
          </div>
     )
